3f386cedda5ccf2d91c34c4107cafb18c8e84954
Mussswe <lylot0t66@gmail.com>
2025-11-05 00:58:31 +0700

    updete milestone close #3

--- ฺbackend/entity/evaluate.go
+++ ฺbackend/entity/evaluate.go
@@ -1,11 +1,7 @@
 package entity
 
-import "time"
-
 type Evaluate struct {
-	ProductID int       `json:"product_id" gorm:"not null"`
-	Rating    int       `json:"rating" gorm:"not null"`
-	Comment   string    `json:"comment" gorm:"type:text"`
-	CreatedAt time.Time `json:"created_at" gorm:"autoCreateTime"`
-	UpdatedAt time.Time `json:"updated_at" gorm:"autoUpdateTime"`
+	ProductID int    `json:"product_id" gorm:"not null"`
+	Rating    int    `json:"rating" gorm:"not null"`
+	Comment   string `json:"comment" gorm:"type:text"`
 }
